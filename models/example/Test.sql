6b1ca5452a02979440ca632a988cea1dd4d4999f
rajashekarreddypr24@gmail.com <rajashekarreddypr24@gmail.com>
2023-03-07 12:36:33 +0000

    aa

--- models/example/Test.sql
+++ models/example/Test.sql
@@ -1,1 +1,9 @@
-select * from test.sheet1
+-- select * from test.sheet1
+
+{{  rename_column(
+      schema = 'test',
+      table = 'sheet1',
+      old_column_name = 'city',
+      new_column_name = 'city1'
+   ) 
+}}
