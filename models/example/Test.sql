70fdf372d710a2f12e66227504a736665e5ba264
Rajashekarreddy24 <prajashekar@stratapps.com>
2023-03-08 12:29:41 +0530

    08/03/2023

--- models/example/Test.sql
+++ models/example/Test.sql
@@ -1,9 +1,3 @@
--- select * from test.sheet1
+select * from test.sheet1
 
-{{  rename_column(
-      schema = 'test',
-      table = 'sheet1',
-      old_column_name = 'city',
-      new_column_name = 'city1'
-   ) 
-}}
+
